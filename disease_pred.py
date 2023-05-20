9c9e9c85bacbb607c332ea7f9fb8339d080f3730
Atharv002 <damleatharv02@gmail.com>
2023-05-20 16:39:41 +0530

    commit

--- disease_pred.py
+++ disease_pred.py
@@ -37,7 +37,7 @@ if (selected == 'Diabetes Prediction'):
     col1, col2, col3 = st.columns(3)
     
     with col1:
-        Pregnancies = st.text_input('Number of Pregnancies')
+        Pregnancies = int(st.text_input('Number of Pregnancies'))
         
     with col2:
         Glucose = st.text_input('Glucose Level')
