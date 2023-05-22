227ff6319352a33a2acac0e4d18b7b22b3773676
Atharv002 <damleatharv02@gmail.com>
2023-05-22 22:43:06 +0530

    commit

--- disease_pred.py
+++ disease_pred.py
@@ -143,6 +143,7 @@ if (selected == 'Heart Disease Prediction'):
           heart_diagnosis = 'The person does not have any heart disease'
         
     st.success(heart_diagnosis)
+    # to print answer after prediction
         
     
     
